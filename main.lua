9ea8dd525221bd51a46c5535198545bc58808fb0
mythinkhouse <pm@ps-MacBook-Air.local>
2017-05-16 11:32:23 -0700

    Additional Icons + Commenting + Clean Up
    Multiple new icons were added, code was cleaned up, notes added.

--- main.lua
+++ main.lua
@@ -1,38 +1,24 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
-
--- Your code here
-
-
 local group = display.newGroup()
 
+--replace this image in the root directory with the icon you want to replicate
 local image = display.newImageRect( "appIconMain.png", 500, 500 )
 image.x = display.contentCenterX
 image.y = display.contentCenterY
 group:insert(image)
 
-local iconName = {"Icon-xxxhdpi", "Icon-xxhdpi", "Icon-xhdpi", "Icon-mdpi", "Icon-ldpi", "Icon-hdpi", "Icon-180", "Icon-167", "Icon-152", "Icon-120", "Icon-87", "Icon-80", "Icon-76", "Icon-58", "Icon-40", "Icon",}
-local iconSize = {192, 144, 96, 48, 36, 72, 180, 167, 152, 120, 87, 80, 76, 58, 40, 57}
+--add new icon names here
+local iconName = {"Icon-Small-40", "Icon-Small-40@2x", "Icon-Small-40@3x", "Icon-Small-50", "Icon-Small-50@2x","Icon-Small-100", "Icon-Small@3x","Icon-Small@2x","Icon-Small","Icon-xxxhdpi", "Icon-xxhdpi", "Icon-xhdpi", "Icon-mdpi", "Icon-ldpi", "Icon-hdpi", "Icon-180", "Icon-167", "Icon-152", "Icon-120", "Icon-87", "Icon-80", "Icon-76", "Icon-58", "Icon-40", "Icon", "Icon@2x"}
+--add new icon sizes here
+local iconSize = {40, 80, 120, 50, 100, 100, 87, 58, 29, 192, 144, 96, 48, 36, 72, 180, 167, 152, 120, 87, 80, 76, 58, 40, 57, 114}
 
 for i = 1, #iconName do 
 
 local function listener( event )
-    print( "listener called" )
-
-print(iconName[i])
-print(iconSize[i])
 image.height = iconSize[i]
 image.width = iconSize[i]
 display.save( group, { filename= iconName[i] .. ".png", baseDir=system.DocumentsDirectory,  } )
-
-
 end
-  
-timer.performWithDelay( 1000 * i, listener )
 
-
+timer.performWithDelay( 300 * i, listener )
 end
 
